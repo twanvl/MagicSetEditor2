26562e03e395ae5832a19bdeccfb28236b4de32c
coppro <coppro@0fc631ac-6414-0410-93d0-97cfa31319b6>
2008-04-06 18:16:32 +0000

    Updated copyright information - added my name and also changed 2007 to 2008
    git-svn-id: svn://svn.code.sf.net/p/magicseteditor/code/trunk@837 0fc631ac-6414-0410-93d0-97cfa31319b6

--- src/script/functions/basic.cpp
+++ src/script/functions/basic.cpp
@@ -1,7 +1,7 @@
 
 //+----------------------------------------------------------------------------+
 //| Description:  Magic Set Editor - Program to make Magic (tm) cards          |
-//| Copyright:    (C) 2001 - 2007 Twan van Laarhoven                           |
+//| Copyright:    (C) 2001 - 2008 Twan van Laarhoven and "coppro"              |
 //| License:      GNU General Public License 2 or later (see file COPYING)     |
 //+----------------------------------------------------------------------------+
 
