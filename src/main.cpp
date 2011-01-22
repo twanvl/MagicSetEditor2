f60842d341a4cdfb3f42c3991ab82ba2922c29ce
twanvl <twanvl@0fc631ac-6414-0410-93d0-97cfa31319b6>
2011-01-22 21:31:45 +0000

    no more handle_warning
    git-svn-id: svn://svn.code.sf.net/p/magicseteditor/code/trunk@1637 0fc631ac-6414-0410-93d0-97cfa31319b6

--- src/main.cpp
+++ src/main.cpp
@@ -67,8 +67,9 @@ void nag_about_ascii_version() {
 		info.dwOSVersionInfoSize = sizeof(info);
 		GetVersionEx(&info);
 		if (info.dwMajorVersion >= 5) {
-			handle_warning(_("This build of Magic Set Editor is intended for Windows 95/98/ME systems.\n")
-			               _("It is recommended that you download the appropriate MSE version for your Windows version."));
+			queue_message(MESSAGE_WARNING,
+				_("This build of Magic Set Editor is intended for Windows 95/98/ME systems.\n")
+				_("It is recommended that you download the appropriate MSE version for your Windows version."));
 		}
 	#endif
 }
