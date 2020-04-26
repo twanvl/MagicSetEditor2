1a39b85b73248e826ce569dc6a728bc55688554e
Twan van Laarhoven <twanvl@gmail.com>
2020-04-27 01:21:18 +0200

    (fix comment)

--- src/data/field.cpp
+++ src/data/field.cpp
@@ -1,4 +1,4 @@
-// //+----------------------------------------------------------------------------+
+//+----------------------------------------------------------------------------+
 //| Description:  Magic Set Editor - Program to make Magic (tm) cards          |
 //| Copyright:    (C) Twan van Laarhoven and the other MSE developers          |
 //| License:      GNU General Public License 2 or later (see file COPYING)     |
